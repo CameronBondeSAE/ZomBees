0dd7b142492471ddcb89f469e08c338e635b4f20
SAE1\1027503 <1027503@student.sae.edu.au>
2023-02-09 13:04:40 +1100

    wandering... maybe still a little too much tho

--- Assets/Team members/Marcus/Steering Tests/GuyDudeWander.cs
+++ Assets/Team members/Marcus/Steering Tests/GuyDudeWander.cs
@@ -2,21 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GuyDudeWander : MonoBehaviour
 {
     private Rigidbody rb;
 
+    private float xOffset;
+    private float zOffset;
+    public float force;
+
     // Start is called before the first frame update
     void Start()
     {
+        xOffset = Random.Range(0, 50);
+        zOffset = Random.Range(0, 50);
+        
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float wander = Mathf.PerlinNoise(Time.time, 0);
-        rb.AddRelativeTorque(0, wander, 0);
+        float x = xOffset + Time.time;
+        float z = zOffset + Time.time;
+        
+        float wander = Mathf.PerlinNoise(x, z) * 2 - 1;
+        rb.AddRelativeTorque(0, wander * force, 0);
     }
 }
