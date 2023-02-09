8b988aab1af92e2d88aa0a77a56f7864614793e4
Cam <30306370+CameronBondeSAE@users.noreply.github.com>
2023-02-09 15:56:53 +1100

    Small test scene

--- Assets/Team members/Cam/Test.cs
+++ Assets/Team members/Cam/Test.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public string thing;
+
+    public void DoThing(string _thing)
+    {
+        Debug.Log(_thing);
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
