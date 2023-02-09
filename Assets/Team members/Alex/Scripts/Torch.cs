0793b7ceaf1bcac6a03716820677e9edc38477e4
Alex <1024087@student.sae.edu.au>
2023-02-09 11:57:48 +1100

    menu stuff

---
+++ Assets/Team members/Alex/Scripts/Torch.cs
@@ -0,0 +1,48 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Alex
+{
+    public class Torch : NetworkBehaviour
+    {
+        public bool isOn = false;
+        public GameObject lightSorce;
+        public AudioSource clickSound;
+  
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(IsLocalPlayer)
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    TurnServerRpc();
+                }
+        }
+
+        [ServerRpc]
+        void TurnServerRpc()
+        {
+            isOn = !isOn;
+        
+            TurnClientRpc(isOn);
+        }
+
+        [ClientRpc]
+        void TurnClientRpc(bool currentTorchState)
+        {
+            if (currentTorchState)
+            {
+                lightSorce.SetActive(true);
+                clickSound.Play();
+                isOn = true;
+            }
+            else
+            {
+                lightSorce.SetActive(false);
+                clickSound.Play();
+                isOn = false;
+            }
+        }
+    }
+}
