08ca7514ce8abdf4d99e06bbfe8fcdb8e8a9f9d4
JohnnyBoii909 <1018518@student.sae.edu.au>
2023-02-15 15:17:36 +1100

    State Manager Getting built

---
+++ Assets/Team members/John/Scripts/StateManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Johns
+{
+    public class StateManager : MonoBehaviour
+    {
+        public MonoBehaviour startingState;
+        public MonoBehaviour currentState;
+ 
+        // Set a default state
+        private void Start()
+        {
+            ChangeState(startingState);
+        }
+
+        // This works for ANY STATE
+        public void ChangeState(MonoBehaviour newState)
+        {
+            // Check if the state is the same and DON'T swap
+            if (newState == currentState)
+            {
+                return;
+            }
+
+            // At first 'currentstate' will ALWAYS be null
+            if (currentState != null)
+            {
+                currentState.enabled = false;
+            }
+
+            newState.enabled = true;
+
+            // New state swap over to incoming state
+            currentState = newState;
+        }
+    }
+}
+
+
