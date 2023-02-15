33b9ad0f78ece3a6650a2a6c8f56a24b28f72e9a
JohnnyBoii909 <1018518@student.sae.edu.au>
2023-02-15 12:57:04 +1100

    Testing push

--- Assets/Team members/John/Scripts/GeneratorOnState.cs
+++ Assets/Team members/John/Scripts/GeneratorOnState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GeneratorOn : MonoBehaviour
 {
@@ -9,23 +10,39 @@ public class GeneratorOn : MonoBehaviour
     public AudioClip generatorShuttingDown;
     
     private AudioSource generatorAudio;
+
+    [SerializeField] private float timePassed;
+    
+    IEnumerator DelayedAction()
+    {
+        yield return new WaitForSeconds(timePassed);
+
+        // Code to execute after a 2 second delay
+    }
     
     
     void OnEnable()
     {
         generatorAudio = GetComponent<AudioSource>();
         generatorAudio.clip = generatorStartUp;
-        
+        Debug.Log("The sound clip playing is "+generatorAudio.clip);
+        timePassed = 3.381f;
+        StartCoroutine(DelayedAction());
+
     }
 
     
     void Update()
     {
         generatorAudio.clip = generatorRunning;
+        Debug.Log("The sound clip playing is "+generatorAudio.clip);
     }
 
     void OnDisable()
     {
         generatorAudio.clip = generatorShuttingDown;
+        Debug.Log("The sound clip playing is "+generatorAudio.clip);
+        timePassed = 5.973f;
+        StartCoroutine(DelayedAction());
     }
 }
