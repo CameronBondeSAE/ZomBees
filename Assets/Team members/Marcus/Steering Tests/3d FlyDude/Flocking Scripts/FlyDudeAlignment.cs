6bfd8f99a2eb1c1ea04503b94d05e23524ba5bb2
SAE1\1027503 <1027503@student.sae.edu.au>
2023-02-22 15:24:13 +1100

    swarming flydudes

--- Assets/Team members/Marcus/Steering Tests/3d FlyDude/Flocking Scripts/FlyDudeAlignment.cs
+++ Assets/Team members/Marcus/Steering Tests/3d FlyDude/Flocking Scripts/FlyDudeAlignment.cs
@@ -6,16 +6,47 @@ namespace Marcus
 {
     public class FlyDudeAlignment : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        // Variable pointing to your Neighbours component
+        public FlyDudeNeighbours neighbours;
+        
+        public float force;
+        Vector3 targetDirection;
+        private Rigidbody rb;
 
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
         }
 
-        // Update is called once per frame
-        void Update()
+        void FixedUpdate()
         {
+            // Some are Torque, some are Force		
+            targetDirection = CalculateMove(neighbours.neighbourDudes);
 
+            // Cross will take YOUR direction and the TARGET direction and turn it into a rotation force vector
+            Vector3 cross = Vector3.Cross(transform.forward, targetDirection);
+
+            rb.AddTorque(cross * force);
+        }
+
+        public Vector3 CalculateMove(List<Transform> neighbours)
+        {
+            if (neighbours.Count == 0)
+            {
+                return Vector3.zero; 
+            }
+
+            Vector3 alignmentMove = Vector3.zero;
+
+            // Average of all neighbours directions
+            // I’m using a list of transforms in my neighbours script, you might be using GameObjects etc
+            foreach (Transform item in neighbours)
+            {
+                alignmentMove += item.transform.forward;
+            }
+
+            alignmentMove /= neighbours.Count;
+            return alignmentMove;
         }
     }
 }
