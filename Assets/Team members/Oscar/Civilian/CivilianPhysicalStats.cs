84dff994af4eceb98a22ab7ec6212621879a2288
Oscar-Skinner <1027514@student.sae.edu.au>
2023-03-01 10:49:03 +1100

    Civilian Physical Stats script start

---
+++ Assets/Team members/Oscar/Civilian/CivilianPhysicalStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilianPhysicalStats : MonoBehaviour
+{
+    public float speed;
+    public float beeness;
+}
