b2eb48f5067d64db537cdca8a2bc9847e0622244
Marcus(Uni) <1027503@student.sae.edu.au>
2023-03-01 13:06:40 +1100

    personality stats

---
+++ Assets/Team members/Marcus/Civillian Stats/CivillianPersonalityStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivillianPersonalityStats : MonoBehaviour
+{
+    public float cowardice;
+    public float sarcasm;
+    public float hostility;
+    //Other things
+}
