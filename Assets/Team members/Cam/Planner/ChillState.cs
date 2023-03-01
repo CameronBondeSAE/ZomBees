b828709dde8afb0b850b41441fccd69c81336bf9
Cam <30306370+CameronBondeSAE@users.noreply.github.com>
2023-03-01 17:37:10 +1100

    Evil AI sounds

--- Assets/Team members/Cam/Planner/ChillState.cs
+++ Assets/Team members/Cam/Planner/ChillState.cs
@@ -5,5 +5,22 @@ using UnityEngine;
 
 public class Chill : AntAIState
 {
+	public CamModel camModel;
+	
+	public override void Create(GameObject aGameObject)
+	{
+		base.Create(aGameObject);
 
+		camModel = aGameObject.GetComponent<CamModel>();
+	}
+
+	public override void Enter()
+	{
+		base.Enter();
+	}
+
+	public override void Exit()
+	{
+		base.Exit();
+	}
 }
