cb580992ad75048a8a58c2da9ab08566e1ed0383
Marcus(Uni) <1027503@student.sae.edu.au>
2023-03-02 12:17:49 +1100

    gridded world, (weird colliders with probuilder)

---
+++ Assets/Team members/Marcus/Pathfinding Stuff/WorldGridding.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Marcus
+{
+    public class WorldGridding : MonoBehaviour
+    {
+        public Vector3Int gridOffset;
+        
+        public Vector3Int worldSize;
+        public Vector3Int gridSize;
+        
+        [ReadOnly][SerializeReference]
+        private bool[,] gridNodeReferences;
+        
+        
+        private void Awake()
+        {
+            gridNodeReferences = new bool[worldSize.x,worldSize.z];
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            /*for (int x = 0; x < worldSize.x; x++)
+            {
+                for (int z = 0; z < worldSize.z; z++)
+                {
+                    if (Physics.OverlapBox(new Vector3(x * gridSize.x, 0, z * gridSize.z),
+                            new Vector3(gridSize.x, gridSize.y, gridSize.z), Quaternion.identity).Length > 0)
+                    {
+                        // Something is there
+                        gridNodeReferences[x, z]/*.isBlocked#1# = true;
+                    }
+                }
+            }*/
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            for (int x = 0; x < worldSize.x; x++)
+            {
+                for (int z = 0; z < worldSize.z; z++)
+                {
+                    if (Physics.OverlapBox(new Vector3(x * gridSize.x, 0, z * gridSize.z) + gridOffset,
+                            new Vector3(gridSize.x, gridSize.y, gridSize.z)/2, Quaternion.identity).Length > 0)
+                    {
+                        // Something is there
+                        gridNodeReferences[x, z]/*.isBlocked*/ = true;
+                    }
+                }
+            }
+        }
+        
+        private void OnDrawGizmos()
+        {
+            for (int x = 0; x < worldSize.x; x++)
+            {
+                for (int z = 0; z < worldSize.z; z++)
+                {
+                    if (gridNodeReferences[x, z]/*.isBlocked*/)
+                    {
+                        Gizmos.color = Color.red;
+                        Gizmos.DrawCube(new Vector3(x, 0, z) + gridOffset, Vector3.one);
+                    }
+                }
+            }
+        }
+    }
+}
