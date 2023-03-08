56d2a76292be1644488f5442bb66e4e4896d589f
Lith_VV <darkninja65786@gmail.com>
2023-03-08 17:06:36 +1100

    started on perlin noise

---
+++ Assets/Team members/Virginia/PerlinD1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinD1 : MonoBehaviour
+{
+    public GameObject brickPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        Y = Mathf.PerlinNoise1D(x);
+        for (int x = 0; x < 100; x++)
+        {
+            Vector3 brickPosition = new Vector3();
+            brickPosition.x = x;
+            brickPosition.y = x;
+            Instantiate(brickPrefab, brickPosition, Quaternion.identity);
+        }
+    }*/
+}
---
+++ Assets/Team members/Virginia/scripts/VPerlin.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+ 
+namespace V {
+public class VPerlin : MonoBehaviour
+{
+        public float offset;
+
+        private void Start()
+        {
+            offset = Random.Range(-10f, 1000f);
+        }
+        void Update()
+    {
+        float y;
+       
+        y = Mathf.Sin(offset  + Time.time * 10f);
+        y += Mathf.Sin(offset + Time.time * 5f);
+        y += Mathf.Sin(offset + Time.time * 2f);
+        y += Mathf.Sin(offset + Time.time * 20f);
+        y = y / 10f;
+
+            transform.localPosition = new Vector3(0, y, 0);
+    }
+}
+}
