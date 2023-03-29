476cbdd412591f2748b2575ca1e62df8f9c1d26a
Cam <30306370+CameronBondeSAE@users.noreply.github.com>
2023-03-29 14:07:03 +1100

    GPT Test scene using new plugin and ChatGPT Shader with C# example

--- Assets/Tutorials/Shader/ShaderPropertiesFromCode.cs
+++ Assets/Tutorials/Shader/ShaderPropertiesFromCode.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ShaderPropertiesFromCode : MonoBehaviour
 {
-	public Renderer renderer;
-	public Texture  tex;
+	public Renderer rend;
+	public Color    myColour;
+
+// Update is called once per frame
+	void Update()
+	{
+		rend.material.SetColor("_Colour", myColour);
+		rend.material.SetFloat("_Blender", Mathf.PerlinNoise1D(Time.time+transform.position.x/10f));
+	}
 
 	// Start is called before the first frame update
     void Start()
     {
-		renderer.material.SetTexture("_Texture2D", tex);
     }
 }
