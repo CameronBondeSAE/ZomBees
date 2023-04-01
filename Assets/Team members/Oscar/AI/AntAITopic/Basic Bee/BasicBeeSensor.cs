2f5a7bd38edc7200ce5d6a51c22f2e75c3134cd0
Oscar-Skinner <1027514@student.sae.edu.au>
2023-04-01 13:35:07 +1100

    NavMesh with basic bee AI

--- Assets/Team members/Oscar/AI/AntAITopic/Basic Bee/BasicBeeSensor.cs
+++ Assets/Team members/Oscar/AI/AntAITopic/Basic Bee/BasicBeeSensor.cs
@@ -21,7 +21,6 @@ namespace Oscar
             aWorldState.Set(BasicBee.SeeFood, controllerAI.seeTheFood());
             aWorldState.Set(BasicBee.HasFood, controllerAI.hasTheFood());
             aWorldState.Set(BasicBee.SeeHive, seeTheHive);
-            aWorldState.Set(BasicBee.NearHive, nearTheHive);
             aWorldState.Set(BasicBee.DeliveredFood, deliveredTheFood);
             aWorldState.Set(BasicBee.StayAlive, beeStayAlive);
             aWorldState.Set(BasicBee.SeeCivilian, controllerAI.seeCivilians());
@@ -35,10 +34,9 @@ namespace Oscar
         SeeFood = 0,
         HasFood = 1,
         SeeHive = 2,
-        NearHive = 3,
-        DeliveredFood = 4,
-        StayAlive = 5,
-        SeeCivilian = 6,
-        EnemyDead = 7
+        DeliveredFood = 3,
+        StayAlive = 4,
+        SeeCivilian = 5,
+        EnemyDead = 6
     }
 }
--- Assets/Team members/Oscar/AI/AntAITopic/Basic Bee/States/DeliverFood.cs
+++ Assets/Team members/Oscar/AI/AntAITopic/Basic Bee/States/DeliverFood.cs
@@ -1,9 +1,84 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Anthill.AI;
+using Oscar;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DeliverFood : AntAIState
 {
+    private LittleGuy littleGuy;
+    private OscarControllerAI controllerAI;
+
+    private GameObject myhome;
     
+    private NavMeshAgent navMeshAgent;
+    private float arrivedDistance = 1.5f;
+
+    private Transform target;
+    private NavMeshPath path;
+    private float elapsed;
+
+    public override void Create(GameObject aGameObject)
+    {
+        base.Create(aGameObject); littleGuy = aGameObject.GetComponent<LittleGuy>();
+        myhome = aGameObject.GetComponent<LittleGuy>().myHome;
+        navMeshAgent = aGameObject.GetComponentInParent<NavMeshAgent>();
+        controllerAI = aGameObject.GetComponent<OscarControllerAI>();
+        
+        path = new NavMeshPath();
+        elapsed = 0.0f;
+
+        TheHive();
+    }
+
+    public Vector3 TheHive()
+    {
+        Vector3 finalDestination = myhome.transform.position;
+        
+        if (controllerAI.hasTheFood() == true)
+        {
+            navMeshAgent.SetDestination(finalDestination);
+        }
+        return finalDestination;
+    }
+
+    public bool ReachedDestinationOrFailed()
+    {
+        if (!navMeshAgent.pathPending)
+        {
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance + arrivedDistance)
+            {
+                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f)
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    public override void Execute(float aDeltaTime, float aTimeScale)
+    {
+        base.Execute(aDeltaTime, aTimeScale);
+        
+        Vector3 theHive = Vector3.zero;
+        if (ReachedDestinationOrFailed())
+        {
+            theHive = TheHive();
+        }
+        
+        elapsed += Time.deltaTime;
+        if (elapsed > 1.0f)
+        {
+            elapsed -= 1.0f;
+            NavMesh.CalculatePath(littleGuy.transform.position, theHive, NavMesh.AllAreas, path);
+        }
+
+        for (int i = 0; i < path.corners.Length - 1; i++)
+        {
+            Debug.DrawLine(path.corners[i],path.corners[i + 1],Color.red, 1f);
+        }
+    }
 }
