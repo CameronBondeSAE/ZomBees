fa115d70a4a2ff382cc45b2e09fde4094a9c46fe
Marcus(Uni) <1027503@student.sae.edu.au>
2023-04-05 15:44:17 +1000

    FINALLY MEMORIES!!!!

--- Assets/Team members/Marcus/Memory Tests/MemoryManager.cs
+++ Assets/Team members/Marcus/Memory Tests/MemoryManager.cs
@@ -8,9 +8,9 @@ namespace Marcus
     public class MemoryManager : MonoBehaviour
     {
         public FoodAIVision vision;
-        public Memory memoryCreator;
-        
         public List<Memory> memories;
+
+        private int counter;
 
         private void OnEnable()
         {
@@ -19,11 +19,32 @@ namespace Marcus
 
         private void AddMemories(GameObject objectSeen)
         {
-            var newMemory = memoryCreator.CreateMemory(objectSeen.GetComponent<FakeDynamicObject>());
+            counter = 0;
 
-            if (!memories.Contains(newMemory))
+            foreach (Memory memory in memories)
             {
+                if (memory.thingToRemember.gameObject == objectSeen)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter <= 0)
+            {
+                var newMemory = gameObject.AddComponent<Memory>().CreateMemory(objectSeen.GetComponent<FakeDynamicObject>());
                 memories.Add(newMemory);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            foreach (Memory memory in memories)
+            {
+                if (Time.time - memory.timeStamp >= 75f)
+                {
+                    memories.Remove(memory);
+                    Destroy(memory);
+                }
             }
         }
     }
