c0ebd425a7ce6b7539df1edd18cce31129360c30
Lith_VV <darkninja65786@gmail.com>
2023-04-06 16:49:40 +1000

    added GetComponent and Physics.OverlapSphere to the Inventory script

--- Assets/Team members/Virginia/scripts/Inventory.cs
+++ Assets/Team members/Virginia/scripts/Inventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using UnityEditor.ShaderGraph.Drawing.Inspector.PropertyDrawers;
 using UnityEngine;
 
 namespace Virginia
@@ -9,11 +10,16 @@ namespace Virginia
     {
         public IItem heldItem;
         [Button] 
-        public void Pickup()
+        public void Pickup(Vector3 center, float radius)
         {
-            Debug.Log("pick up");
-            
-           
+            Collider[] ItemFound = Physics.OverlapSphere(center, radius);
+            foreach (var Itemsfound in ItemFound)
+            { 
+                GetComponent<IItem>();   
+                Debug.Log(message: "pick up");
+            }
+
+
         }
         
         [Button] 
@@ -25,6 +31,7 @@ namespace Virginia
         public void Dispose()
         {
             Debug.Log("disposed item didn't need it");
+
         }
         
         
