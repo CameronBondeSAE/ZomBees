143e89230a0ad61dde1dbc0d786151615bcabb09
Lith_VV <darkninja65786@gmail.com>
2023-04-12 14:27:53 +1000

    fixed

--- Assets/Team members/Virginia/scripts/PerlinSpawner.cs
+++ Assets/Team members/Virginia/scripts/PerlinSpawner.cs
@@ -25,14 +25,13 @@ namespace Virginia
         {
             for (int z = 0; z < 100; z++)
             {
-                for (int x = 0; //size.x; x = x+Cubesize)
+                for (int x = 0; x < 100; x++)
                 {
                     Vector3 BeeHivepos = new Vector3();
                     BeeHivepos.x = x;
+                    BeeHivepos.y = Mathf.PerlinNoise1D(x);
                     BeeHivepos.z = z;
-                    //BeeHivepos.y = Mathf.PerlinNoise1D(x);
                     Instantiate(BeeHive, BeeHivepos, quaternion.identity);
-
 
                 }
             }
