1e87cf5642dc04c54a2e3a96075fa0e45c40a72e
Lith_VV <darkninja65786@gmail.com>
2023-04-13 14:27:43 +1000

    changed the switch

--- Assets/Team members/Virginia/scripts/Switch/SwitchOn.cs
+++ Assets/Team members/Virginia/scripts/Switch/SwitchOn.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
+
 using Virginia;
 namespace Virginia
 {
@@ -14,7 +14,7 @@ namespace Virginia
             
             //TurnOnEvent?.Invoke();
             Switch.ThingToSwitch?.TurnOn();
-           transform.DOLocalMove(new Vector3(-2, 1, -7), 1f);
+       
             GetComponent<Renderer>().material.color = Color.green;
         }
 
