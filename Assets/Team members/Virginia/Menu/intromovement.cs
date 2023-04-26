8ebd9714b039597b67bb6122167dbf75d5440682
Lith_VV <darkninja65786@gmail.com>
2023-04-26 16:58:00 +1000

    started on the menu and placed them into a folder called menu

---
+++ Assets/Team members/Virginia/Menu/intromovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class intromovement : MonoBehaviour
+{
+   
+    void Start()
+   // DOTween.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
