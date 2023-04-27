33e4a05458777a085cd6667997b1b1135ac2ea9b
Lith_VV <darkninja65786@gmail.com>
2023-04-27 16:58:26 +1000

    bee bg has been replaced with clean one and image can fade in and out

--- Assets/Team members/Virginia/Menu/intromovement.cs
+++ Assets/Team members/Virginia/Menu/intromovement.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
 
-
-public class intromovement : MonoBehaviour
+namespace Virginia
 {
-   
-    void Start()
-   {
-   // DOTween.
-    }
-
-    // Update is called once per frame
-    void Update()
+    public class IntroMovement : MonoBehaviour
     {
-        
+        public Image image;
+        void Start()
+        {
+            image.transform.localScale = Vector3.one; // justs sets the cover at 1,1,1 at the start
+            Sequence mySequence = DOTween.Sequence();
+            mySequence.Append(image.DOColor(Color.black, 0f)); // starts off black then switches 
+            mySequence.Append(image.DOColor(Color.white, 2f));
+            mySequence.Insert(0, image.transform.DOScale(new Vector3(2.5f, 2.5f, 2.5f), 3f));
+            mySequence.AppendCallback(PressMe);
+            mySequence.Play();
+
+
+            void PressMe()
+            {
+                Debug.Log("I should have play");
+
+            }
+            // Update is called once per frame
+            void Update()
+            {
+
+            }
+        }
     }
 }
