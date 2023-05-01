a35e2cae697c2671a5c219c7d6e9790eefc5f39d
MarcusGeorgiou <1027503@student.sae.edu.au>
2023-05-01 14:09:12 +1000

    Started hide state, fixed a few things with AI prefab and movement, removed run from planner

--- Assets/Team members/Marcus/Planner Stuff/States/Hide.cs
+++ Assets/Team members/Marcus/Planner Stuff/States/Hide.cs
@@ -2,21 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using Anthill.AI;
 using UnityEngine;
+using Oscar;
 
 namespace Marcus
 {
     public class Hide : AntAIState
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        private AdvancedGuyDudeMovement move;
+        private OscarVision vision;
 
+        private Vector3 averageEnemyPosition;
+        private PatrolPoint chosenPoint;
+        
+        public override void Create(GameObject aGameObject)
+        {
+            base.Create(aGameObject);
+
+            move = aGameObject.GetComponentInParent<AdvancedGuyDudeMovement>();
+            vision = aGameObject.GetComponentInChildren<OscarVision>();
         }
 
-        // Update is called once per frame
-        void Update()
+        public override void Enter()
         {
+            base.Enter();
+            
+            CalculateEnemyAveragePosition();
+            
+            // If there is a nearby point within a threshold, go there without hesitation
+            float distance = Vector3.Distance(transform.position, averageEnemyPosition);
 
+            Collider[] nearbyPoints =
+                Physics.OverlapSphere(transform.position, distance / 3f, LayerMask.NameToLayer("Patrol points"));
+
+            if (nearbyPoints.Length > 0)
+            {
+                chosenPoint = 
+                    nearbyPoints[Random.Range(0, nearbyPoints.Length)].GetComponent<PatrolPoint>();
+                ChangeTargetPoint();
+            }
+            // Else, Take the average position of enemies in view
+            // Find the nearest point in the opposite direction
+            // Go towards it
+            else
+            {
+                /*
+                TODO Figure out a calculation for opposite direction
+                
+                TODO Set "chosenPoint" to a hiding spot in this direction
+                chosenPont = 
+                
+                ChangeTargetPoint();
+                */
+            }
         }
+
+        public override void Exit()
+        {
+            base.Exit();
+
+            chosenPoint = null;
+            ChangeTargetPoint();
+        }
+
+        #region Functions
+
+        void ChangeTargetPoint()
+        {
+            move.MoveToPoint(chosenPoint);
+        }
+
+        void CalculateEnemyAveragePosition()
+        {
+            Vector3 sum = Vector3.zero;
+            foreach (DynamicObject bee in vision.beesInSight)
+                sum += bee.transform.position;
+
+            averageEnemyPosition = sum / vision.beesInSight.Count;
+        }
+
+        #endregion
     }
 }
