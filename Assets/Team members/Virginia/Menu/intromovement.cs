76b6c5297f580097d80ccc62b1ae3c3a47489a6c
Lithtinia <darkninja65786@gmail.com>
2023-05-04 16:53:03 +1000

    health supplies
    the health supplies should now be able to working fingers crossed

--- Assets/Team members/Virginia/Menu/intromovement.cs
+++ Assets/Team members/Virginia/Menu/intromovement.cs
@@ -23,7 +23,7 @@ namespace Virginia
             
             mySequence.Append(image.DOColor(Color.black, 0f)); // starts off black then switches 
             mySequence.Append(image.DOColor(Color.white, 2f));
-            mySequence.Insert(0, image.transform.DOScale(new Vector3(2.5f, 2.5f, 2.5f), 3f));
+            mySequence.Insert(0, image.transform.DOScale(new Vector3(3f, 3f, 3f), 3f));
             //this makes the image expand in size by 2.5 times
             
             mySequence.Play();
--- Assets/Team members/Virginia/scripts/HealthSupplies.cs
+++ Assets/Team members/Virginia/scripts/HealthSupplies.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Lloyd;
 using Oscar;
 using UnityEngine;
 
@@ -7,24 +8,34 @@ namespace Virginia
 {
     public class HealthSupplies : DynamicObject, IItem
     {
+        GameObject   whoPickedMeUp;
+        public float healthAmount = 50;
+        public TraitScriptableObject scriptableObject;
+
+        public void Pickup(GameObject _whoPickedMeUp)
+        {
+            whoPickedMeUp                         = _whoPickedMeUp;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Collider>().enabled      = false;
+        }
+
+
         public void Consume()
         {
-           
+            whoPickedMeUp.GetComponent<Health>().Change(healthAmount);
+            whoPickedMeUp.GetComponent<CivilianTraits>().UpdateTrait(scriptableObject, 0f);
+            Destroy(gameObject);
         }
 
         public void Dispose()
         {
-         
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Collider>().enabled      = true;
         }
 
         public string Description()
         {
-            return ("healthy");
-        }
-
-        public void Pickup(GameObject whoPickedMeUp)
-        {
-            
+            return description;
         }
     }
 }
